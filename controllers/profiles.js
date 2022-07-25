d59d58b1f588aa42e8c7d50260feb87972874d39
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-25 12:52:33 -0400

    add route and controller fn to get profile data

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,20 +1,9 @@
 import { Profile } from '../models/profile.js'
 
-
-function index(req, res) {
-  Profile.find({})
-  .then(profiles => res.json(profiles))
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
-}
-
 function addTask(req, res) {
   Profile.findById(req.user.profile)
   .then(profile => {
     profile.tasks.push(req.body)
-    console.log(profile)
     profile.save()
     .then(updatedProfile => {
       res.json(updatedProfile)
@@ -26,7 +15,18 @@ function addTask(req, res) {
   })
 }
 
+function showProfile(req, res) {
+  Profile.findById(req.user.profile)
+  .then(profile => {
+    res.json(profile)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({ err: err.errmsg })
+  })
+}
+
 export { 
-  index,
-  addTask
+  addTask,
+  showProfile
 }
