11e9a47903a4c0307c042dc9955e380daf063b05
Zach Eggert <egger0a6@email.arizona.edu>
2022-07-27 03:19:21 -0400

    update controller fn

--- controllers/tips.js
+++ controllers/tips.js
@@ -2,7 +2,18 @@ import { Tip } from '../models/tip.js'
 
 function create(req, res) {
   req.body.owner = req.user.profile
-  console.log(req.body)
+  Tip.create(req.body)
+  .then(tip => {
+    Tip.findById(tip._id)
+    .populate("owner")
+    .then(populatedTip => {
+      res.json(populatedTip)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 export { 
